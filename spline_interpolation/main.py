c2a35ce1b6e14ef80160c092a0eee2f465c64a02
Mihas <lemiasheuski@student.agh.edu.pl>
2022-06-07 18:08:11 +0200

    spline interpolation todo

--- spline_interpolation/main.py
+++ spline_interpolation/main.py
@@ -1,153 +1,108 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def defineX(startPoint,endPoint,numOfPoints):
-    xPoints = []
-    for i in range (numOfPoints +1):
-        xPoints.append(startPoint + (endPoint-startPoint)/numOfPoints*i)
-    return xPoints
-
-
-
-def defineY(xPoints,func):
-    yPoints = []
-    for x in xPoints:
-        yPoints.append(func(x))
-    return yPoints
-
-
-
-def defineLambda(xPoints):
-    lambdas = []
-    for i in range (1,len(xPoints)):
-        lambdas.append(xPoints[i] - xPoints[i-1])
-    return lambdas
-
-
-
-def defineMu(lamdas):
-    mu = []
-    for x in lambdas:
-        mu.append(1-x)
-    return mu
-
-
-
 def func(x):
     return 1/(1+x**2)
 
 
-def generateMatrix(n,lambdas,mu):
-    matrix = np.zeros(shape=(n,n))
-    for i in range (0,n):
+def generateLambdas(h_array):
+    lambdas = []
+    for i in range(len(h_array) - 1):
+        lambdas.append(h_array[i+1]/(h_array[i] + h_array[i+1]))
+    return lambdas
 
-        for j in range (0,n):
-            if (i == j):
-                if (i!=0 and i != n-1):
-                    matrix[i][j] = 2
-                else:
-                    matrix[i][j] = 1
-            elif (i == j-1 and i != 0):
-                matrix[i][j] = lambdas[i]
-            elif (i == j + 1):
-                matrix[i][j] = mu[i]
 
-    matrix[-1][-2] = 0
+def generateH(x_array):
+    h_array = []
+    for i in range(len(x_array)-1):
+        h_array.append(x_array[i+1] - x_array[i]) 
+    return h_array
+
+
+def generateMu(lambda_array):
+    mu = []
+    for item in lambda_array:
+        mu.append(1 - item)
+    return mu
+
+
+def generateX(begin,end,num_of_points):
+    x_points = []
+    for i in range(num_of_points+1):
+        x_points.append(begin + (end - begin)/num_of_points*i)
+    return x_points
+
+
+def calculateY(x_points,func):
+    y_points = []
+    for x in x_points:
+        y_points.append(func(x))
+    return y_points
+
+
+# def generateM():
+#     pass
+
+
+def calculate_d_column(h,y):
+    d_column = [0] #!!!!! alpha = beta = 0
+    for i in range(1,len(h)-1):
+        d_column.append(6/(h[i] + h[i+1]) * ((y[i+1] - y[i])/h[i+1] - (y[i] - y[i-1])/h[i]) )
+    d_column.append(0)
+    return d_column
+
+
+def A_i(i,y,h,m):
+    return (y[i] - y[i-1])/h[i] - h[i]/6 * (m[i] - m[i-1])
+
+
+def B_i(y_prev,m_prev,h_i):
+    return y_prev - m_prev * (h_i ** 2)/6
+
+
+def generate_matrix_A(mu,_lambda,num_of_points):
+    matrix = np.zeros((num_of_points,num_of_points))
+    matrix[0][0] = matrix[-1][-1] = 1
+    for i in range (1,num_of_points-1):
+        matrix[i][i] = 2
+        matrix[i][i-1] = mu[i-1]
+        matrix[i][i+1] = _lambda[i-1]
     return matrix
 
 
-
-def defineFreeElemVector(xPoints,yPoints):
-    d = [0]
-    for i in range (1,len(xPoints)-2):
-        d.append(6/(xPoints[i+1] - xPoints[i-1])*((yPoints[i+1] - yPoints[i])/(xPoints[i+1]-xPoints[i]) - (yPoints[i] - yPoints[i-1])/(xPoints[i] - xPoints[i-1])))
-    d.append(0)
-    return d
-
-
-
-def calculateInterpolatedFunction(func,startPoint,endPoint,numOfPoints,mVector):
-    xPoints = defineX(startPoint,endPoint,numOfPoints)
-    yPoints = defineY(xPoints,func)
-    step = startPoint
-    values = []
+def calculate_s_i(m,x_points,h,y):
+    x = x_points[0]
+    s_array=[]
     i = 1
-    while step < endPoint:
-        if (step > xPoints[i]):
-            i += 1
-            if (xPoints[i] == endPoint):
-                break
-        print(step)
-        values.append(mVector[i-1]*((xPoints[i] - step)**3)/(6*(xPoints[i] - xPoints[i-1])) + mVector[i]*((step - xPoints[i-1])**3)/(6*(xPoints[i] - xPoints[i-1])) + ((yPoints[i] - yPoints[i-1])/(xPoints[i] - xPoints[i-1]) - (xPoints[i] - xPoints[i-1])/6*(mVector[i] - mVector[i-1]))*(step - xPoints[i-1]) + yPoints[i-1] - mVector[i-1]*(xPoints[i] - xPoints[i-1])**2/6)
-        step += 0.1
-    return values
-
-
-def drawPolynomial(yPoints,startPoint,endPoint):
-    step = startPoint
-    xPoints = []
-    while step <= endPoint:
-        xPoints.append(step)
-        step += 0.1
-    plt.plot(xPoints,yPoints)
-    plt.show()
-
-
-def drawFunc(startPoint,endPoint,func):
-    step = startPoint;
-    xPoints = []
-    yPoints = []
-    while step <= endPoint:
-        xPoints.append(step)
-        yPoints.append(func(step))
-        step += 0.1
-    plt.ylim(-0.2,3)
-    plt.plot(xPoints,yPoints)
-    # plt.show()
-
-
+    while x <= x_points[-1]:
+        s_array.append(m[i-1] * ((x_points[i] - x)**3)/(6 * h[i]) + m[i]*(x-x[i-1])**3/(6*h[i]) + A_i(i,y,h,m) * (x - x_points[i-1]) + B_i(y[i-1],m[i-1],h[i]))
+    return s_array
 
 
 
 if __name__ == "__main__":
 
-    startPoint = -5
-    endPoint = 5
-    numOfPoints = 5
+    x_min = -5
+    x_max = 5
+    num_of_points = 10
 
-    xPoints = defineX(startPoint,endPoint,numOfPoints)
-    yPoints = defineY(xPoints,func)
-    lambdas = defineLambda(xPoints)
-    mu = defineMu(lambdas)
+    x_points = generateX(x_min,x_max,num_of_points)
+    y_points = calculateY(x_points,func)
 
-    # print(xPoints)
-    # print(yPoints)
+    h_array = generateH(x_points)
+    lambda_array = generateLambdas(h_array)
+    mu_array = generateMu(lambda_array)
 
+    d_column = calculate_d_column(h_array,y_points)
+    matrix_A = generate_matrix_A(mu_array,lambda_array,num_of_points)
 
-    # print(mu)
-    # print(lambdas)
+    # print(d_column)
+    # print(matrix_A)
 
-    # print(defineY(defineX(startPoint,endPoint,numOfPoints),func))
+    m_column = np.linalg.solve(matrix_A,d_column)
 
-    matrix = generateMatrix(numOfPoints,lambdas,mu)
-    # print(matrix)
+    print(m_column)
 
-    freeVector = defineFreeElemVector(xPoints,yPoints)
-    # print(freeVector)
+    
 
-    mVector = np.linalg.solve(matrix,freeVector)
-    # print(mVector)
-
-    polynomialValues = calculateInterpolatedFunction(func,startPoint,endPoint,numOfPoints,mVector)
-
-    # print(polynomialValues)
-
-    drawFunc(startPoint,endPoint,func)
-    drawPolynomial(polynomialValues,startPoint,endPoint)
-
-
-
-
-
-
+    # print(generateMu(generateLambdas(generateH(generateX(x_min,x_max,num_of_points)))))
