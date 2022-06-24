f46909b82baf523fbe9bf3b2488ec5b8ce9a3009
albusDalbador <lemiasheuski@student.agh.edu.pl>
2022-06-24 19:52:03 +0200

    testing secion added

--- pseudorandom_number_generator/main.py
+++ pseudorandom_number_generator/main.py
@@ -1,8 +1,7 @@
 from cmath import sqrt
 from math import *
-from optparse import Values
+from statistics import variance
 import matplotlib.pyplot as plt
-from collections import Counter
 
 
 def probability_density(x,sigma,mu):
@@ -21,15 +20,39 @@ def calculate_autocorrelation(values,shift,variation,mu): #serival correlation
         sum += (values[i]-mu) * (values[i+shift]-mu)
     return sum/((len(values) - shift)*variation**2)
 
-# def draw_histogram(elements):
-#     plt.hist(elements)
-#     plt.show()
-
-def draw_coverage(x_points): 
+def scatter_coverage(x_points): 
     plt.scatter(x_points[:-1],x_points[1:],s=1)
     plt.show()
 
+def draw_histogram(values):
+    plt.hist(values,rwidth=0.9,bins=30)
+    plt.show()
 
+def calculate_values_in_intervals(values,sigma,mu):
+    intervals = [0]*12
+    for x in values:
+        x_scaled = (x-mu)/(6*sigma) + sigma
+        intervals[floor(x_scaled*12)] += 1
+    return intervals
+
+def calcualte_testing_statistics(intervals,sigma,mu):
+    n = sum(intervals)
+    start = -3*sigma + mu
+    step = 6*sigma/ 12
+    x_square = 0.0
+    out = open("testing_output.dat","a")
+    out.write("\nstep values from test statistic calculation\n")
+    for i in range (0,12):
+        p_i = probability_density(start+(i+1)*step,sigma,mu) - probability_density(start+i*step,sigma,mu)
+        x_square += pow(intervals[i]-n*p_i,2)/(n*p_i)
+        out.write(f'p_i = {round(p_i,5)}, n*p_i = {round(n*p_i,5)}\n')
+    out.close()
+    return x_square
+
+
+
+
+#2.1
 def uniform_distribution_algorithm():
     x_prev = 10
     for (a,c,m) in ((123,1,2**15),(69069,1,2**32)):
@@ -42,43 +65,53 @@ def uniform_distribution_algorithm():
         interval_statistic = [0]*12
         for x in x_array_normalized:
             interval_statistic[floor(x*12)] += 1
-        print(calculate_autocorrelation(x_array_normalized,1000,calculate_vatiation(x_array_normalized),0.5))
 
-        plt.hist(x_array_normalized,rwidth=0.95,bins=20)
-        plt.show()
+        # draw_histogram(x_array_normalized)
+        # scatter_coverage(x_array_normalized)
 
 
-def normal_distribution_algorithm():
+#2.2
+def normal_distribution_algorithm() -> list:
     a = 69069
     c = 1
     m = 2**32
     mu = 0.2
     sigma = 0.5
     random_values = []
-    # y_random = 10
-    # x_random = 10
     random_seed = 10
     while len(random_values) < 10**4:
         x_random = random_seed = (a*random_seed + c) % m
         y_random = random_seed = (a*random_seed + c) % m
-        x_random_scaled = (x_random/(m+1) - 0.5)*3*sigma + mu
+        x_random_scaled = (x_random/(m+1) - 0.5)*6*sigma + mu
         y_random_scaled = y_random/(m+1)/(sigma*sqrt(2*pi))
         if (y_random_scaled < probability_density(x_random_scaled,sigma,mu)):
             random_values.append(x_random_scaled)
-    plt.hist(random_values,bins=20)
-    plt.show()
+    # draw_histogram(random_values)
+    return random_values
 
 
-def testing():
-    pass
+#2.3
+def testing(values,sigma,mu):
+    average = sum(values)/len(values)
+    variation = calculate_vatiation(values)
+    open("testing_output.dat","w").close()
+    with open("testing_output.dat","a") as out:
+        out.write(f'\nvariation : {variation}\naverage value : {average}\n')
+
+    interval_statistics = calculate_values_in_intervals(values,sigma,mu)
+    with open("testing_output.dat","a") as out:
+        out.write("\nnumber of values in 12 subranges:\n" + ", ".join(list(map(str,interval_statistics))) + "\n")
+
+    testing_scatistics = calcualte_testing_statistics(interval_statistics,sigma,mu)
+    print(testing_scatistics)
 
 
 
 
 if __name__ == "__main__":
     
-    # uniform_distribution_algorithm()
+    uniform_distribution_algorithm()
 
-    normal_distribution_algorithm()
+    normal_dist_points = normal_distribution_algorithm()
 
-    testing()
+    testing(normal_dist_points,0.5,0.2)
